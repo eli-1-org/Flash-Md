f68eeb054964b4b868081ba2e6e4202ef69b824d
eli-1-org <elizabethopeyemiayeni@gmail.com>
2024-07-23 13:28:59 +0100

    Update set.js

--- set.js
+++ set.js
@@ -14,7 +14,7 @@ module.exports = { session: process.env.SESSION_ID || 'FLASH-MD-WA-BOT;;;=>eyJub
              
     AUTO_READ_STATUS: process.env.AUTO_VIEW_STATUS || "on",
 AUTOREAD_MESSAGES: process.env.AUTO_READ_MESSAGES || "off",
-CHATBOT: process.env.CHAT_BOT || "on",
+CHATBOT: process.env.CHAT_BOT || "off",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_SAVE_STATUS || 'off',
     BOT : process.env.BOT_NAME || 'FLASH-MD',
     //OPENAI_API_KEY : process.env.OPENAI_API_KEY || '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',
